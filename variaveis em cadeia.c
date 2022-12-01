0b7a5338ba37673b44b36a771c52327dc15c5c24
Gabriel Martins <104040977+MartinsGabrielR@users.noreply.github.com>
2022-12-01 15:26:08 -0300

    Add files via upload

---
+++ variaveis em cadeia.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+printf("cotacao em relacao ao real 01/12/2022\n");
+//variaveis
+float dolar = 5.19;
+float  euro = 5.45;
+float   BTC = 88025.65;
+
+
+printf("Dolar: %0.2f\nEuro: %f0.2\nBTC: %0.2f\n",dolar,euro,BTC);
+
+    return 0;
+
+}
