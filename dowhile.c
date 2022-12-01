85c8cd8ad832e0e8c7ee03e8bcbc0abc0b10ce2a
Gabriel Martins <104040977+MartinsGabrielR@users.noreply.github.com>
2022-12-01 18:55:39 -0300

    Add files via upload

---
+++ dowhile.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int i =0;
+
+while (i != 0)//verifica e depois faz
+{
+printf("tstet");
+}
+
+
+do // primeiro ele faz e depois ele ele verifica 
+{
+    printf("teste 2\n");
+} while (i!=0);
+
+
+
+    return 0;
+}
