bf3a7c3a78b3590123ab10787deacdd2be5a98f0
Gabriel Martins <104040977+MartinsGabrielR@users.noreply.github.com>
2022-12-01 14:57:18 -0300

    Add files via upload

---
+++ cometarios.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+// fazer um comentario
+/*
+posso escrever oq eu quiser aqui
+que vai continuar sendo um 
+cometario
+*/
+printf("testando comentario.\n");
+
+    return 0;
+}
