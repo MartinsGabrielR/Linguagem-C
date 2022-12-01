7369dd4596a409a0215b028b49c739d92abb8925
Gabriel Martins <104040977+MartinsGabrielR@users.noreply.github.com>
2022-12-01 17:45:34 -0300

    Add files via upload

---
+++ while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int contador = 1;
+
+    while (contador <= 5)//enquanto isso não acontecer fazer isso até acontecer
+    {
+        printf("%i\n",contador);
+        contador++;
+    }
+    
+
+    return 0;
+}
