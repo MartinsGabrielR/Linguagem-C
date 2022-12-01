c0cde7f05de3be87f6c88ddfa8914b9e838627e6
Gabriel Martins <104040977+MartinsGabrielR@users.noreply.github.com>
2022-12-01 17:31:33 -0300

    Add files via upload

---
+++ laco.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+int contador;
+int numero =1;
+
+for (contador = 1/*dando valor ao contador*/; contador <= 10/*limite*/; contador++/*contador = contador + 1*/ ){
+//tudo que estiver dentro dos colchetes vai ser executado 
+
+printf("teste\n"); // vai imprimir essa tela pro usuario 10 vezes
+printf("brasil"); // vai imprimir mais 10 vezes 
+// total, vao imprimir 20 vezes
+
+}
+
+
+    return 0;
+}
