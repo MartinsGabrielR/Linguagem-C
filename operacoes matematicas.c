2e739d7de8bbaccacae6a06079c46a69deaa1adc
Gabriel Martins <104040977+MartinsGabrielR@users.noreply.github.com>
2022-12-01 15:48:21 -0300

    Add files via upload

---
+++ operacoes matematicas.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+double soma;
+printf(":::Ganhos:::\n");
+
+    double dia1 = 33.15;
+    double dia2 = 69.29;
+    double dia3 = 87.36;
+    double dia4 = 13.53;
+    double dia5 = 52.30;
+
+     soma = dia1 + dia2 + dia3 + dia4+ dia5;
+
+     printf("Ganhos totais: %0.2lf$\n",soma);
+
+
+    return 0;
+}
